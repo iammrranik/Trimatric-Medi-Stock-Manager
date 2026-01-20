7e7db0dd4e6bc5d3ebe1f662260db08623be1320
Mrr Anik <74314368+iammrranik@users.noreply.github.com>
2026-01-20 22:03:21 +0600

    Added Implement fetchAll and deleteRecord functions
    Added generic fetch and delete functions for database operations.

--- model/adminModel.php
+++ model/adminModel.php
@@ -1,4 +1,19 @@
 <?php
 include_once 'conModel.php';
 
+// READ: Generic fetch for any table
+function fetchAll($table) {
+    global $conn;
+    return mysqli_query($conn, "SELECT * FROM " . mysqli_real_escape_string($conn, $table));
+}
+
+// DELETE: Generic delete using specific primary keys
+function deleteRecord($table, $column, $value) {
+    global $conn;
+    $table = mysqli_real_escape_string($conn, $table);
+    $column = mysqli_real_escape_string($conn, $column);
+    $value = mysqli_real_escape_string($conn, $value);
+    return mysqli_query($conn, "DELETE FROM $table WHERE $column = '$value'");
+}
+
 ?>
