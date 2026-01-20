23dfce1b3ba3876194e45683fd42c7ffbf858225
Mrr Anik <74314368+iammrranik@users.noreply.github.com>
2026-01-20 22:33:48 +0600

    Added logout, delete, and save user functionality in adminController
    Added logout, delete, and save user logic to admin controller.

--- controller/adminController.php
+++ controller/adminController.php
@@ -3,4 +3,37 @@ session_start();
 include_once '../model/adminModel.php';
 
 
+// 1. LOGOUT LOGIC
+if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+    session_destroy();
+    setcookie("user_login", "", time() - 3600, "/");
+    setcookie("user_role", "", time() - 3600, "/");
+    header("Location: ../view/loginPage.php");
+    exit();
+}
+
+// 2. DELETE LOGIC (All Tables)
+if (isset($_GET['del']) && isset($_GET['type'])) {
+    $type = $_GET['type'];
+    $val = $_GET['del'];
+
+    if ($type == 'users') deleteRecord('users', 'username', $val);
+    elseif ($type == 'inv') deleteRecord('inventory', 'id', $val);
+    elseif ($type == 'pat') deleteRecord('patients', 'patient_serial', $val);
+    elseif ($type == 'pay') deleteRecord('payments', 'payment_id', $val); 
+
+    header("Location: ../view/adminPage.php?msg=deleted");
+    exit();
+}
+
+// 3. SAVE / UPDATE LOGIC (Routing to Model Functions)
+
+// Process User
+if (isset($_POST['save_user'])) {
+    saveUser($_POST['old_username'], $_POST);
+    header("Location: ../view/adminPage.php?msg=success");
+    exit();
+}
+
+
 ?>
