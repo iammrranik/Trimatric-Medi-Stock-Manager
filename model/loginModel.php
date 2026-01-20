5da8ebd8674f977a374644f549eb39b28a64506e
ONIK <mahedihasanonik21@gmail.com>
2026-01-20 21:46:29 +0600

    Clean up whitespace in loginModel.php
    Removed an extraneous newline for cleaner code formatting.

--- model/loginModel.php
+++ model/loginModel.php
@@ -12,7 +12,6 @@ function validateUser($username, $password) {
     
     $sql = "SELECT username, password, role FROM users WHERE username = '$user' LIMIT 1";
     $result = mysqli_query($conn, $sql);
-
     if ($result && mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
         
