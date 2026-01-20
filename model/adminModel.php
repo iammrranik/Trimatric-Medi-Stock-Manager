a08cf5c3eff1e7c7778f1e58a0359595279ef0bc
Mrr Anik <74314368+iammrranik@users.noreply.github.com>
2026-01-20 22:04:11 +0600

    Added saveUser function for user management in adminModel.php

--- model/adminModel.php
+++ model/adminModel.php
@@ -16,4 +16,25 @@ function deleteRecord($table, $column, $value) {
     return mysqli_query($conn, "DELETE FROM $table WHERE $column = '$value'");
 }
 
+// SAVE USER (8 Columns)
+function saveUser($old_user, $d) {
+    global $conn;
+    $u = mysqli_real_escape_string($conn, $d['username']);
+    $f = mysqli_real_escape_string($conn, $d['full_name']);
+    $n = mysqli_real_escape_string($conn, $d['nid']);
+    $e = mysqli_real_escape_string($conn, $d['email']);
+    $a = mysqli_real_escape_string($conn, $d['address']);
+    $p = mysqli_real_escape_string($conn, $d['password']);
+    $ph = mysqli_real_escape_string($conn, $d['phone_no']);
+    $r = mysqli_real_escape_string($conn, $d['role']);
+
+    if (empty($old_user)) {
+        return mysqli_query($conn, "INSERT INTO users VALUES ('$u','$f','$n','$e','$a','$p','$ph','$r')");
+    } else {
+        return mysqli_query($conn, "UPDATE users SET username='$u', full_name='$f', nid='$n', email='$e', address='$a', password='$p', phone_no='$ph', role='$r' WHERE username='$old_user'");
+    }
+}
+
+
+
 ?>
